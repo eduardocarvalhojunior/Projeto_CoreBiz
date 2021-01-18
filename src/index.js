0dc353a63edd70bbac50e185eb942be207642663
eduardocarvalhojunior <junior.baller@hotmail.com>
2021-01-18 01:15:49 -0300

    Atualizando 18/01/2021

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { render } from "react-dom";
+import "./index.css";
+import Contador from "./Contador";
+import Titulos from "./Titulos";
+render(
+  <React.StrictMode>
+    <Titulos />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
